ca58f6985322f0824ea7a0562933687034c6c0f5
Electrostats <Super_justy@hotmail.com>
2024-06-13 15:47:26 -0400

    Full implementation (but its ugly for now)

--- PracticeManager.cs
+++ PracticeManager.cs
@@ -1,42 +1,107 @@
 ﻿using HarmonyLib;
-using Mono.Security.Protocol.Tls;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TootTallyCore;
+using TootTallyCore.Graphics;
 using TootTallyCore.Utils.TootTallyGlobals;
+using TootTallyCore.Utils.TootTallyNotifs;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Localization.Components;
 using UnityEngine.UI;
 
 namespace TootTallyPractice
 {
     public static class PracticeManager
     {
+        private static LevelSelectController _currentInstance;
         private static Button _practiceButton;
+        private static AudioSource _btnClickSfx;
+        private static PracticePanel _practicePanel;
+        public static float StartTime;
 
         [HarmonyPatch(typeof(LevelSelectController), nameof(LevelSelectController.Start))]
         [HarmonyPostfix]
         public static void OnLevelSelectControllerStartPostfixAddPracticeButton(LevelSelectController __instance)
         {
+            _currentInstance = __instance;
+            TootTallyGlobalVariables.isPracticing = false;
+            StartTime = 0;
+            _practicePanel = new PracticePanel();
+
+            //Practice Button Setup
+            _btnClickSfx = __instance.hoversfx;
             _practiceButton = GameObject.Instantiate(__instance.playbtn, __instance.fullpanel.transform);
             _practiceButton.name = "PRACTICE";
             var rect = _practiceButton.GetComponent<RectTransform>();
             rect.anchoredPosition = new Vector2(456, -58);
-            var txtObj = _practiceButton.transform.Find("txt-play");
-            txtObj.GetComponent<Text>().text = txtObj.transform.GetChild(0).GetComponent<Text>().text = "PRACTICE";
-            
+            var txts = _practiceButton.GetComponentsInChildren<Text>();
+            foreach (var txt in txts)
+            {
+                if (txt.TryGetComponent<LocalizeStringEvent>(out var localStringEvent))
+                    GameObject.DestroyImmediate(localStringEvent);
+                txt.text = "<i>Practice</i>";
+            }
+            _practiceButton.onClick = new Button.ButtonClickedEvent();
+            _practiceButton.onClick.AddListener(OnPracticeButtonClick);
+            var btnTriggers = _practiceButton.GetComponent<EventTrigger>();
+            btnTriggers.triggers.Clear();
+
+            EventTrigger.Entry pointerEnterEvent = new EventTrigger.Entry();
+            pointerEnterEvent.eventID = EventTriggerType.PointerEnter;
+            pointerEnterEvent.callback.AddListener(OnPracticeButtonHover);
+            btnTriggers.triggers.Add(pointerEnterEvent);
+
+            EventTrigger.Entry pointerLeaveEvent = new EventTrigger.Entry();
+            pointerLeaveEvent.eventID = EventTriggerType.PointerExit;
+            pointerLeaveEvent.callback.AddListener(OnPracticeButtonUnhover);
+            btnTriggers.triggers.Add(pointerLeaveEvent);
         }
 
-        public static void OnPracticeButtonHover()
+        public static void OnPracticeButtonHover(BaseEventData _)
         {
-
+            _btnClickSfx.Play();
+            _practiceButton.transform.Find("playBackground").GetComponent<Image>().color = Theme.colors.playButton.backgroundOver;
+            _practiceButton.transform.Find("playOutline").GetComponent<Image>().color = Theme.colors.playButton.outlineOver;
+            _practiceButton.transform.Find("txt-play/txt-play-front").GetComponent<Text>().color = Theme.colors.playButton.textOver;
+            _practiceButton.transform.Find("playShadow").GetComponent<Image>().color = Theme.colors.playButton.shadowOver;
         }
 
-        public static void OnPracticeButtonUnhover()
+        public static void OnPracticeButtonUnhover(BaseEventData _)
         {
+            _practiceButton.transform.Find("playBackground").GetComponent<Image>().color = Theme.colors.playButton.background;
+            _practiceButton.transform.Find("playOutline").GetComponent<Image>().color = Theme.colors.playButton.outline;
+            _practiceButton.transform.Find("txt-play/txt-play-front").GetComponent<Text>().color = Theme.colors.playButton.text;
+            _practiceButton.transform.Find("playShadow").GetComponent<Image>().color = Theme.colors.playButton.shadow;
+        }
+
+        public static void OnPracticeButtonClick()
+        {
+            _practicePanel.Show(_currentInstance.alltrackslist[_currentInstance.songindex].trackref);
+        }
+
+        public static void StartSongWithPractice()
+        {
+            TootTallyGlobalVariables.isPracticing = true;
+            _currentInstance.clickPlay();
 
         }
 
+
+        [HarmonyPatch(typeof(LevelSelectController), nameof(LevelSelectController.advanceSongs))]
+        [HarmonyPrefix]
+        public static bool SkipAdvanceSongsIfPracticePanelVisible() => !_practicePanel.IsPracticePanelVisible;
+
+        [HarmonyPatch(typeof(GameController), nameof(GameController.playsong))]
+        [HarmonyPrefix]
+        public static void SkipToStartTimeOnSongStart(GameController __instance)
+        {
+            if (!TootTallyGlobalVariables.isPracticing) return;
+
+            if (StartTime > __instance.musictrack.clip.length - 1) StartTime = __instance.musictrack.clip.length - 1;
+            __instance.musictrack.time = StartTime;
+            __instance.track_xpos_fixedperframe = __instance.zeroxpos + StartTime * -__instance.trackmovemult;
+            __instance.track_xpos_smoothscrolling = __instance.track_xpos_fixedperframe;
+            __instance.noteholderr.anchoredPosition3D = new Vector3((float)__instance.track_xpos_fixedperframe, 0f, 0f);
+        }
     }
 }
---
+++ PracticePanel.cs
@@ -0,0 +1,98 @@
+﻿using BaboonAPI.Hooks.Tracks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using TootTallyCore.Graphics;
+using TootTallyCore.Utils.TootTallyNotifs;
+using UnityEngine;
+using UnityEngine.Localization.Components;
+using UnityEngine.UI;
+
+namespace TootTallyPractice
+{
+    public class PracticePanel
+    {
+        private GameObject _practicePanel;
+        private Text _title;
+        private TMP_Text _startTimeLabel;
+        private GameObject _panelContainer;
+        private Slider _startTimeSlider;
+        public bool IsPracticePanelVisible;
+        private bool _isStarting;
+
+        public PracticePanel()
+        {
+            //Panel content
+            _practicePanel = GameObjectFactory.CreateOverlayPanel(null, Vector2.zero, new Vector2(1000, 350), 12f, "PracticePanel");
+            _practicePanel.SetActive(false);
+            var fgPanel = _practicePanel.transform.GetChild(0).GetChild(1);
+            GameObject.DestroyImmediate(fgPanel.GetChild(1).gameObject);
+            _title = fgPanel.GetChild(0).GetComponent<Text>();
+            GameObject.DestroyImmediate(_title.GetComponent<LocalizeStringEvent>());
+            _panelContainer = fgPanel.GetChild(1).gameObject;
+            _startTimeLabel = GameObjectFactory.CreateSingleText(_panelContainer.transform, "StartTime", "Start Time: 0:00");
+            _startTimeLabel.rectTransform.sizeDelta = new Vector2(600, 40);
+            _startTimeSlider = GameObjectFactory.CreateSliderFromPrefab(_panelContainer.transform, "StartTimeSlider");
+            _startTimeSlider.transform.localScale = Vector3.one;
+            _startTimeSlider.GetComponent<RectTransform>().sizeDelta = new Vector2(700, 40);
+            _startTimeSlider.handleRect.sizeDelta = new Vector2(40, 0);
+            _startTimeSlider.gameObject.SetActive(true);
+            _startTimeSlider.onValueChanged.AddListener(OnStartTimeSliderValueChange);
+
+            GameObjectFactory.CreateCustomButton(_panelContainer.transform, Vector2.zero, new Vector2(120, 40), "Back", "PracticeBackBtn", Hide);
+            GameObjectFactory.CreateCustomButton(_panelContainer.transform, Vector2.zero, new Vector2(120, 40), "Start", "PracticeStartBtn", StartSong);
+
+            //Panel setup
+            _title.text = "Practice Mode";
+            var panelRect = _practicePanel.GetComponent<RectTransform>();
+            panelRect.anchorMin = panelRect.anchorMax = panelRect.pivot = Vector2.one / 2f;
+            var containerRect = _panelContainer.GetComponent<RectTransform>();
+            containerRect.anchoredPosition = Vector2.zero;
+            containerRect.sizeDelta = new Vector2(1000, 150);
+            var containerLayout = _panelContainer.GetComponent<VerticalLayoutGroup>();
+            containerLayout.childAlignment = TextAnchor.MiddleCenter;
+            containerLayout.padding = new RectOffset();
+
+            IsPracticePanelVisible = false;
+        }
+
+        public void Show(string trackref)
+        {
+            var track = TrackLookup.tryLookup(trackref);
+            if (track == null)
+            {
+                TootTallyNotifManager.DisplayNotif("Track Incompatible with practice mode");
+                return;
+            }
+
+            SetSliderMaxValue(track.Value.length - 1);
+            _startTimeSlider.value = 0;
+            IsPracticePanelVisible = true;
+            _practicePanel.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            IsPracticePanelVisible = false;
+            _practicePanel.SetActive(false);
+        }
+
+        public void StartSong()
+        {
+            if (_isStarting) return;
+            _isStarting = true;
+            PracticeManager.StartSongWithPractice();
+        }
+
+        public void OnStartTimeSliderValueChange(float value)
+        {
+            PracticeManager.StartTime = value;
+            _startTimeLabel.text = $"Start Time: {TimeSpan.FromSeconds(value):mm\\:ss} / {TimeSpan.FromSeconds(_startTimeSlider.maxValue):mm\\:ss}";
+        }
+
+        public void SetSliderMaxValue(float maxValue) => _startTimeSlider.maxValue = maxValue;
+    }
+}
